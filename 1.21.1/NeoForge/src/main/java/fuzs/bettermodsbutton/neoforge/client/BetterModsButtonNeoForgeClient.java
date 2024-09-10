fc6caf49e53b2a8b34c30a8311d095d97fabbc3f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-09-10 14:38:29 +0200

    full 1.21.1 port

--- 1.21.1/NeoForge/src/main/java/fuzs/bettermodsbutton/neoforge/client/BetterModsButtonNeoForgeClient.java
+++ 1.21.1/NeoForge/src/main/java/fuzs/bettermodsbutton/neoforge/client/BetterModsButtonNeoForgeClient.java
@@ -2,15 +2,22 @@ package fuzs.bettermodsbutton.neoforge.client;
 
 import fuzs.bettermodsbutton.BetterModsButton;
 import fuzs.bettermodsbutton.client.handler.ModsButtonHandler;
+import fuzs.bettermodsbutton.neoforge.client.config.ConfigTranslationsManager;
 import fuzs.bettermodsbutton.services.ClientAbstractions;
 import net.minecraft.DetectedVersion;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.PreparableReloadListener;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfigs;
+import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
 import net.neoforged.neoforge.client.event.ScreenEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.forge.snapshots.ForgeSnapshotsMod;
 import net.neoforged.neoforge.internal.BrandingControl;
@@ -23,16 +30,25 @@ import java.util.Collections;
 public class BetterModsButtonNeoForgeClient {
 
     public BetterModsButtonNeoForgeClient(ModContainer modContainer) {
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        ModConfigs.getModConfigs(BetterModsButton.MOD_ID).forEach(ConfigTranslationsManager::addModConfig);
+        registerLoadingHandlers(modContainer.getEventBus());
         registerEventHandlers(NeoForge.EVENT_BUS);
+    }
+
+    private static void registerLoadingHandlers(IEventBus eventBus) {
+        eventBus.addListener((final RegisterClientReloadListenersEvent evt) -> {
+            ConfigTranslationsManager.onAddResourcePackReloadListeners(
+                    (ResourceLocation resourceLocation, PreparableReloadListener preparableReloadListener) -> {
+                        evt.registerReloadListener(preparableReloadListener);
+                    });
+        });
     }
 
     private static void registerEventHandlers(IEventBus eventBus) {
         eventBus.addListener((final ScreenEvent.Init.Post evt) -> {
-            ModsButtonHandler.onAfterInitScreen(evt.getScreen().getMinecraft(),
-                    evt.getScreen(),
-                    evt.getListenersList(),
-                    evt::addListener,
-                    evt::removeListener
+            ModsButtonHandler.onAfterInitScreen(evt.getScreen().getMinecraft(), evt.getScreen(), evt.getListenersList(),
+                    evt::addListener, evt::removeListener
             );
             setCollapsedBrandingControl(evt.getScreen());
         });
